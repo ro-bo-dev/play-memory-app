26966ce3903f1b63169242ce9bc5fcd1e4bb6b1e
ro-bo-dev <robert.borgmann@outlook.com>
2023-04-23 07:42:30 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'playMemoryApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
